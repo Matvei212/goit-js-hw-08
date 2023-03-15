5521c060c9552875d6948cb8343bf50833580105
Matvei212 <q841479@gmail.com>
2023-03-15 13:46:02 +0200

    v6

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,65 @@
+import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items';
-
-const divGalleryRef = document.querySelector(".gallery");
-
-const galleryMarkupRef = createGalleryMarkup(galleryItems);
-
-divGalleryRef.insertAdjacentHTML("beforeend", galleryMarkupRef);
-
-function createGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-      <a class="gallery__item" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>
-        `;
-    })
-    .join("");
-}
-new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
 // Change code below this line
+
+
+console.log(galleryItems);
+
+
+const galleryListEl = document.querySelector(".gallery");
+galleryListEl.setAttribute('uk-lightbox', 'caption-position:bottom');
+
+console.log(galleryListEl)
+
+galleryListEl.addEventListener('click', onGalleryListElClick);
+
+function createGalleryElement(array) {
+    
+   return array.map(({ preview, original, description }) => {
+        const item = document.createElement('a');
+        
+        item.href = original;
+       item.classList.add('gallery__item');
+       item.dataset.caption = description;
+        const image = document.createElement("img");
+        image.src = preview;
+        image.classList.add('gallery__image');
+       image.alt = description;
+       image.title = description;
+       image.delay = 250;
+       image.titlePosition = 'top';
+        item.append(image);
+        
+       return item;
+    })
+   
+}
+
+const galleryElements = createGalleryElement(galleryItems);
+galleryListEl.append(...galleryElements);
+
+
+function onGalleryListElClick(e) {
+  
+    
+    
+    if (e.target.nodeName !== "IMG") {
+        return
+    };
+    
+
+    let href = (e.target.closest('a').getAttribute('href'));
+    return href;
+ 
+}
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.on('show.simplelightbox', function () {
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e);
+});
 
 console.log(galleryItems);
